9c0b39edce6665bd158cb3ae2c0487d07e7c2028
vshamr <vero94@mail.ru>
2022-01-11 00:25:13 +0300

    add: tweets layout

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,19 @@
-import {Grid, IconButton, Typography, makeStyles, Container} from "@material-ui/core";
-import TwitterIcon from '@mui/icons-material/Twitter';
-import SearchIcon from '@mui/icons-material/Search';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-import MailIcon from '@mui/icons-material/MailOutline';
-import BookmarkIcon from '@mui/icons-material/BookmarkBorder';
-import ListIcon from '@mui/icons-material/FormatListBulleted';
-import PersonIcon from '@mui/icons-material/PersonOutline';
+import {Grid, IconButton, Typography, makeStyles, Container, withStyles, Paper, Avatar} from "@material-ui/core";
+import {createStyles, InputBase} from "@mui/material";
+import classNames from 'classnames';
+import TwitterIcon from "@mui/icons-material/Twitter";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import MailIcon from "@mui/icons-material/MailOutline";
+import BookmarkIcon from "@mui/icons-material/BookmarkBorder";
+import ListIcon from "@mui/icons-material/FormatListBulleted";
+import PersonIcon from "@mui/icons-material/PersonOutline";
+import CommentIcon from '@mui/icons-material/ChatBubbleOutline';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import LikeIcon from '@mui/icons-material/FavoriteBorder';
+import ReplyIcon from '@mui/icons-material/Reply';
+import grey from "@material-ui/core/colors/grey";
+
 
 const useStylesHome = makeStyles(() => ({
     wrapper: {
@@ -34,8 +42,47 @@ const useStylesHome = makeStyles(() => ({
     },
     sideMenuListItemIcon: {
         fontSize: 25,
-    }
-}))
+    },
+    tweetsWrapper: {
+        borderRadius: 0,
+        height: '100%',
+        borderTop: '0',
+        borderBottom: '0',
+    },
+    tweetsHeader: {
+        borderTop: '0',
+        borderLeft: '0',
+        borderRight: '0',
+        borderRadius: 0,
+        padding: '10px 15px',
+        '& h6': {
+            fontWeight: 800,
+        },
+    },
+    tweet: {
+      '&: hover': {
+       backgroundColor: 'rgb(245, 248, 250)',
+      },
+    },
+    tweetsFooter: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        width: 450,
+    },
+    tweetsUserName: {
+        color: grey[500],
+    },
+}));
+
+const SearchTextField = withStyles(() =>
+    createStyles({
+        input: {
+            borderRadius: 30,
+            backgroundColor: '#E6ECF0',
+            height: 45,
+            padding: 0,
+        }
+    }))(InputBase);
 
 export const Home = () => {
     const classes = useStylesHome();
@@ -90,10 +137,58 @@ export const Home = () => {
 
                 </Grid>
                 <Grid item xs={6}>
-
+                    <Paper className={classes.tweetsWrapper} variant="outlined">
+                        <Paper className={classes.tweetsHeader} variant="outlined">
+                            <Typography variant="h6">Главная</Typography>
+                        </Paper>
+                        <Paper className={classNames(classes.tweet, classes.tweetsHeader)} variant="outlined">
+                            <Grid container spacing={3}>
+                                <Grid item xs={1}>
+                                    <Avatar
+                                        alt="User Avatar"
+                                        src="http://s1.iconbird.com/ico/0612/practika/w256h2561339698323user.png"
+                                    />
+                                </Grid>
+                                <Grid item xs={11}>
+                                    <Typography>
+                                        <b>Maksym</b> <span className={classes.tweetsUserName}>@maksym24</span>
+                                    </Typography>
+                                    <Typography variant="body1" gutterBottom>
+                                        В моем роду было целое поколение художников от рождения. Они все умерли от
+                                        алкоголизма. Ген художника мне не передался, как видите, а вот...
+                                    </Typography>
+                                    <div className={classes.tweetsFooter}>
+                                        <div>
+                                            <IconButton>
+                                                <CommentIcon style={{fontSize: 20}}/>
+                                            </IconButton>
+                                            <span>1</span>
+                                        </div>
+                                        <div>
+                                            <IconButton>
+                                                <RepeatIcon style={{fontSize: 20}}/>
+                                            </IconButton>
+                                        </div>
+                                        <div>
+                                            <IconButton>
+                                                <LikeIcon style={{fontSize: 20}}/>
+                                            </IconButton>
+                                        </div>
+                                        <div>
+                                            <IconButton>
+                                                <ReplyIcon style={{fontSize: 20}}/>
+                                            </IconButton>
+                                        </div>
+                                    </div>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    </Paper>
                 </Grid>
                 <Grid item xs={3}>
-
+                    <SearchTextField
+                        placeholder="Поиск по Твиттеру"
+                        fullWidth/>
                 </Grid>
             </Grid>
         </Container>
